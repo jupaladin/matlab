64d58868d6cf215329ae1849673e70b36f9186dc
jupaladin <paladin_zxy@hotmail.com>
2016-12-23 12:34:01 +0100

    matlab
    read two matrices from the .txt documents

---
+++ ReadMatrix.m
@@ -0,0 +1,8 @@
+function[A,B]=ReadMatrix(file1,file2)
+%% Function description
+% ReadMatrix is the function of reading matrices 
+% file1 and file2 are the two matrices to be read in this function
+%% ReadMatrix
+A=load(file1)
+B=load(file2)
+end
